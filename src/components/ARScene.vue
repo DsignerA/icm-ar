1786e6d7c72ede50d1118b6120944dcc3acfe330
https://github.com/DsignerA <darrylarrington9@gmail.com>
2024-05-24 19:01:29 -0400

    Added AR.js and A-Frame dependencies

--- src/components/ARScene.vue
+++ src/components/ARScene.vue
@@ -15,10 +15,13 @@
 
     <div style='margin: 0px; overflow: hidden;'>
       <a-scene 
-        embedded
-        arjs="sourceType: webcam; debugUIEnabled: false;"
+        loading-screen="dotsColor: white; backgroundColor: black" 
+        id="scene" 
+        cursor="fuse: false; rayOrigin: mouse;" 
+        embedded 
+        arjs="sourceType: webcam; detectionMode: mono; maxDetectionRate: 30; canvasWidth: 240; canvasHeight: 180; debugUIEnabled: false;" 
         vr-mode-ui="enabled: false"
-        renderer="logarithmicDepthBuffer: true;"
+        arjs-gesture-detector
       >
         <a-assets>
           <a-asset-item id="apt1" src="https://rvanfts.com/ar/1-branded-chips/scene.gltf"></a-asset-item>
@@ -27,11 +30,31 @@
 
         <template v-if="defaultSceneVisible">
           <a-marker preset="hiro" id="marker1">
-            <a-entity raycaster="objects: .clickable" position="0 0 0" id="myapt1" scale="1 1 1" rotation="-50 0 0" gltf-model="#apt1" class="clickable" visible="true" gesture-handler></a-entity>
+            <a-entity 
+              raycaster="objects: .clickable" 
+              position="0 0 0" 
+              id="myapt1" 
+              scale="1 1 1" 
+              rotation="-50 0 0" 
+              gltf-model="#apt1" 
+              class="clickable" 
+              visible="true" 
+              gesture-handler
+            ></a-entity>
           </a-marker>
 
           <a-marker type="pattern" url="https://rvanfts.com/ar/1patterns/main-ar.patt" id="marker2">
-            <a-entity raycaster="objects: .clickable" position="0 0 0" id="myapt2" scale="1 1 1" rotation="-50 0 0" gltf-model="#apt2" class="clickable" visible="true" gesture-handler></a-entity>
+            <a-entity 
+              raycaster="objects: .clickable" 
+              position="0 0 0" 
+              id="myapt2" 
+              scale="1 1 1" 
+              rotation="-50 0 0" 
+              gltf-model="#apt2" 
+              class="clickable" 
+              visible="true" 
+              gesture-handler
+            ></a-entity>
           </a-marker>
         </template>
 
@@ -267,7 +290,6 @@ export default {
     },
     async loadMarkers() {
       const scene = document.querySelector('a-scene');
-      const assets = document.querySelector('a-assets');
       const objectsRef = db.collection('objects');
       try {
         const snapshot = await objectsRef.get();
@@ -303,29 +325,24 @@ export default {
     }
   },
   mounted() {
-  this.loadMarkers();
+    this.loadMarkers();
 
-  const loader = document.getElementById('loader');
-  const myapt = document.getElementById('myapt');
-  if (myapt) {
+    const loader = document.getElementById('loader');
+    const myapt = document.getElementById('myapt');
     myapt.addEventListener('model-loading', () => {
       loader.style.display = 'block';
       myapt.setAttribute('visible', 'false');
     });
-
     myapt.addEventListener('model-loaded', () => {
       loader.style.display = 'none';
       myapt.setAttribute('visible', 'true');
     });
+
+    this.checkAuthState();
   }
-
-  this.checkAuthState();
-}
-
 };
 </script>
 
 <style>
 /* Include your styles here */
 </style>
-
