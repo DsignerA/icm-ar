eb4dbed8be6cd1359ec2d2b54e60ca4cb2b5a06c
https://github.com/DsignerA <darrylarrington9@gmail.com>
2024-06-02 19:41:01 -0400

    sidebar login

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -8,34 +8,12 @@
     </div>
   </template>
   <script>
-  import firebase from 'firebase/app';
-  import 'firebase/auth';
-  import 'firebase/firestore';
-  
-  const firebaseConfig = {
-    apiKey: "AIzaSyBp_DolCW24dLRggJ79qfsB6lKrkJ8osrQ",
-    authDomain: "arwallet-bfd5e.firebaseapp.com",
-    projectId: "arwallet-bfd5e",
-    storageBucket: "arwallet-bfd5e.appspot.com",
-    messagingSenderId: "865160955063",
-    appId: "1:865160955063:web:e82191fc0207faf1fcb932",
-    measurementId: "G-LV113YTC27"
-  };
-  
-  // Initialize Firebase
-  if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-  }
-  
-  const auth = firebase.auth();
-  const db = firebase.firestore();
+  import { auth, db } from './firebase.js';
   
   export default {
     data() {
       return {
         sidebarWidth: '0',
-        allowClicks: true,
-        currentItem: null,
       };
     },
     methods: {
