6f9cd92356795c39e58388dfe1e82907f2798ecc
https://github.com/DsignerA <darrylarrington9@gmail.com>
2024-06-04 12:33:01 -0400

    sidebar login

--- src/components/ARScene.vue
+++ src/components/ARScene.vue
@@ -19,6 +19,21 @@
 </template>
 
 <script>
+const firebaseConfig = {
+  apiKey: "AIzaSyBp_DolCW24dLRggJ79qfsB6lKrkJ8osrQ",
+  authDomain: "arwallet-bfd5e.firebaseapp.com",
+  projectId: "arwallet-bfd5e",
+  storageBucket: "arwallet-bfd5e.appspot.com",
+  messagingSenderId: "865160955063",
+  appId: "1:865160955063:web:e82191fc0207faf1fcb932",
+  measurementId: "G-LV113YTC27"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+
 export default {
   data() {
     return {
